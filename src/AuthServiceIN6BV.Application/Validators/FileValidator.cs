1605a2455adf456810c1ed7b980498a5bcba9c26
fpaiz-2024264 <fpaiz-2024264@kinal.edu.gt>
2026-02-03 17:21:53 -0600

    add: Se continuo el trabajo en clase el 3/02/2026

---
+++ src/AuthServiceIN6BV.Application/Validators/FileValidator.cs
@@ -0,0 +1,50 @@
+using AuthServiceIN6BV.Application.Interfaces;
+ 
+namespace AuthServiceIN6BV.Application.Validators;
+ 
+public static class FileValidators
+{
+    private static readonly string[] AllowedImageExtensions = [".jpg", ".jpeg", ".png", ".webp"];
+ 
+    private const int MaxFilesSizeInBytes = 5 * 1024 * 1024; //5mb
+ 
+    public static (bool IsValid, string? ErrorMessage) ValidateImage(IFileData file)
+    {
+        if (file == null || file.Size == 0)
+        {
+            return(false, "File is required.");
+        }
+ 
+        if (file.Size > MaxFilesSizeInBytes)
+        {
+            return(false, $"File size cannot exceed {MaxFilesSizeInBytes / (1024 * 1024)}MB");
+        }
+ 
+        var extension = Path.GetExtension(file.FileName).ToLowerInvariant();
+ 
+        if (!AllowedImageExtensions.Contains(extension))
+        {
+            return(false,$"Only the following file types are allowed: {string.Join(", ")}");
+
+        }
+
+        //Validar tipo de archivo
+        var allowebContentTypes = new [] {"image/jpg", "image/jpg", "image/png", "image/webp"};
+        if (!allowebContentTypes.Contains(file.ContentType.ToLowerInvariant()))
+        {
+            return (false, "Invalid file type");
+
+        }
+
+        return (true,null);
+ 
+       
+ 
+    }
+    public static string GenerateSecureFileName(string originalFileName)
+    {
+        var extension = Path.GetExtension(originalFileName).ToLowerInvariant();
+        var uniqueId = Guid.NewGuid().ToString("N")[..12];
+        return $"profile-{uniqueId}{extension}";
+    }
+}
